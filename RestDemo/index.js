c9e86b556d45df218f46975038a8a889bae02934
oscarvalerio <oscarvalerio@gmail.com>
2022-08-14 19:27:27 -0500

    updates to handle json files

--- RestDemo/index.js
+++ RestDemo/index.js
@@ -2,12 +2,16 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 app.get('/tacos', (req, res)=> {
     res.send("GET /tacos response")
 })
 
 app.post('/tacos', (req, res)=> {
-    res.send("POST /tacos response")
+    const {meat,qty} = req.body;
+    res.send(`Ok, here are your ${qty} ${meat} tacos`)
 })
 
 app.listen(3000, ()=> {
