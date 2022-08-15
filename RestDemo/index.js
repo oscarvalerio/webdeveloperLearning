0f48c84afb1a946638c2fef635b7c0ae8bc654c6
oscarvalerio <oscarvalerio@gmail.com>
2022-08-14 19:08:11 -0500

    Initial repo

---
+++ RestDemo/index.js
@@ -0,0 +1,16 @@
+//Require and execute express
+const express = require('express');
+const app = express();
+
+app.get('/tacos', (req, res)=> {
+    res.send("GET /tacos response")
+})
+
+app.post('/tacos', (req, res)=> {
+    res.send("POST /tacos response")
+})
+
+app.listen(3000, ()=> {
+    console.log('Listening on port 3000')
+}
+)
