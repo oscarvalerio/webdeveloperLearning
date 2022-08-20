964b5a906b00ea1c14fcf2f1300dd457be6387cc
oscarvalerio <oscarvalerio@gmail.com>
2022-08-19 21:24:47 -0500

    update for initial commit

--- RestDemo/index.js
+++ RestDemo/index.js
@@ -1,9 +1,36 @@
 //Require and execute express
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.set('views', path.join(__dirname,'views'))
+app.set('view engine','ejs')
+
+const comments = [
+    {
+        username: 'Todd',
+        comment: 'lol thats so funny'
+    },
+    {
+        username: 'Skyler',
+        comment: 'I like my dog'
+    },
+    {
+        username: 'Sk8erBoi',
+        comment: 'Please delete your email todd'
+    },
+    {
+        username: 'onlysayswoof',
+        comment: 'woof woof woof'
+    }
+]
+
+//Set the CRUD resources for the comments functionality
+app.get('/comments',(req, res)=>{
+    res.render('comments/index')
+})
 
 app.get('/tacos', (req, res)=> {
     res.send("GET /tacos response")
