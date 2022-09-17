cd8df7f342f4aaa72d972826e75d4bc3cd21b552
oscarvalerio <oscarvalerio@gmail.com>
2022-09-17 13:21:56 -0500

    Adding pages for new and list all products

--- MongooseBasic/Mongoose_Express/index.js
+++ MongooseBasic/Mongoose_Express/index.js
@@ -20,10 +20,38 @@ mongoose.connect('mongodb://localhost:27017/farmStand', {useNewUrlParser: true, 
 //Express server code to start server connection
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
+app.use(express.urlencoded({extended: true}));
 
-app.get('/dog',(req,res)=>{
-    res.send('Woof')
+//respond with a page that contains all the products
+app.get('/products', async (req,res)=>{
+    //Get all the products
+    const products = await Product.find({})
+    //display index.ejs and pass all products as a parameter
+    res.render('products/index', {products})
 })
+
+//respond with a form to create a new product
+app.get('/products/new', (req, res)=>{
+    res.render('products/new')
+})
+
+//catch the value of the form
+app.post('/products', async (req,res)=>{
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`)
+})
+
+//respond with a details page for every product
+app.get('/products/:id', async (req,res)=>{
+    //destructure the response to get the iD
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    console.log(product);
+    res.render('products/show', {product})
+
+})
+
 
 app.listen(3000, ()=>{
     console.log("APP IS LISTENING ON PORT 3000!")
