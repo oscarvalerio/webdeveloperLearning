9c30f9982cc44e74ee01e9e45cccc8bce90beead
oscarvalerio <oscarvalerio@gmail.com>
2022-10-02 18:19:04 -0500

    Updates to index file

--- exercises_yelpcamp/index.js
+++ exercises_yelpcamp/index.js
@@ -11,6 +11,23 @@ app.use((req, res, next)=>{
     next();
 });
 
+//You can use the method USE to run on a specific path too
+app.use('/dogs',(req,res,next)=>{
+    console.log("I Love Dogs");
+    next();
+});
+
+const verifyPassword= ((req,res,next)=>{
+    //Get the query string from the browser
+    const { password } = req.query;
+    //If the password is the same as expected, then continue
+    //Else, send an error
+    if(password==="chickennugget"){
+        next()
+    }
+    res.send("SORRY YOUR PASSWORD IS INCORRECT");
+});
+
 app.get('/', (req, res)=>{
     //Login the time of the request we got from the app.use method
     console.log(`REQUESTED DATE: ${req.requestTime}`);
@@ -21,6 +38,17 @@ app.get('/dogs', (req,res) =>{
     //Login the time of the request we got from the app.use method
     console.log(`REQUESTED DATE: ${req.requestTime}`);
     res.send('Woof Woof');
+});
+
+//If you want to verify the password in a specific page add it as callback
+app.get('/secret',verifyPassword, (req,res)=>{
+    res.send("My Secret is: I hate shower");
+});
+
+//You can also define app.use to create a Not Found page
+//Just define the function at the end of the code
+app.use((req,res)=>{
+    res.status(404).send("Page Not Found")
 });
 
 app.listen(3000, ()=>{
